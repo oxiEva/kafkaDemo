45b737a474098d854ef2ec1dd326bcbe4cd92c2e
Eva Lujan Cardenas <elujan@hiberus.com>
2025-02-27 15:53:28 +0100

    Add Kafka consumer and provider modules

---
+++ provider/src/test/java/com/kafka/provider/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.provider.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
